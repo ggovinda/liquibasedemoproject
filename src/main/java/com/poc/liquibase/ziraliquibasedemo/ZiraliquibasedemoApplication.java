58b7f5c9ead63bcc7ed62c32b0f55064b51dc4fd
Gokul <gokulraj.govindaraj@telenetgroup.be>
2022-12-02 11:49:06 +0530

    First commit of the liquibase poc changes

---
+++ src/main/java/com/poc/liquibase/ziraliquibasedemo/ZiraliquibasedemoApplication.java
@@ -0,0 +1,13 @@
+package com.poc.liquibase.ziraliquibasedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZiraliquibasedemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZiraliquibasedemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/poc/liquibase/ziraliquibasedemo/ZiraliquibasedemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poc.liquibase.ziraliquibasedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZiraliquibasedemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
